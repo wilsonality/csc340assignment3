ca79cdd88e030e7d63886d58609d016a4cc79d8f
wilsonality <ptcisbored@gmail.com>
2025-10-08 00:22:38 -0400

    fixing ignore issues again

---
+++ src/main/java/com/wilsonality/animalsCrudAPI/AnimalsCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.wilsonality.animalsCrudAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilsonality/animalsCrudAPI/AnimalsCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilsonality.animalsCrudAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
